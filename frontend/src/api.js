1e74265b5ecf49d753b3aabb1557370c2562e7bb
Palak Tiwari <palaktiwari4300@gmail.com>
2026-04-23 13:27:08 +0530

    check

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://lostfound-backend-lwfx.onrender.com"
+  baseURL: "https://lostfound-backend-lwfx.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
